f53e684576b07a63060490a3c4884f76008b4ff9
unknown <itnek0pi@gmail.com>
2020-02-10 22:44:13 +0200

    #Breakout - little changes to todo list

--- breakout/todolist.lua
+++ breakout/todolist.lua
@@ -2,10 +2,11 @@
 Welcome to your third assignment! By now, we’ve gotten our feet wet with states, randomization, and much more; this time, 
 we’ll be diving in a little bit more and adding some new features entirely!
 
+? Need to have BetterComments plugin to make comments look a bit more bright
 ! Your goals this assignment:
 
 
-Todo Add a Powerup class to the game that spawns a powerup 
+todo / Add a Powerup class to the game that spawns a powerup 
 (images located at the bottom of the sprite sheet in the distribution code). 
 * This Powerup should spawn randomly, be it on a timer or when the Ball hits a Block enough times, and 
 gradually descend toward the player. Once collided with the Paddle, two more Balls should spawn and behave 
@@ -13,14 +14,14 @@ identically to the original, including all collision and scoring points for the 
 proceeds to the VictoryState for their current level, the Balls should reset so that there is only one active again.
 
 
-todo Grow and shrink the Paddle such that it’s no longer just one fixed size forever. 
+todo / Grow and shrink the Paddle such that it’s no longer just one fixed size forever. 
 In particular, the Paddle should shrink if the player loses a heart (but no smaller of course than the smallest paddle size) 
 and should grow if the player exceeds a certain amount of score (but no larger than the largest Paddle). This may not make the game 
 completely balanced once the Paddle is sufficiently large, but it will be a great way to get comfortable interacting with 
 Quads and all of the tables we have allocated for them in main.lua!
 
 
-todo Add a locked Brick (located in the sprite sheet) to the level spawning, as well as a key powerup 
+todo / Add a locked Brick (located in the sprite sheet) to the level spawning, as well as a key powerup 
 (also in the sprite sheet). The locked Brick should not be breakable by the ball normally, unless they of course 
 have the key Powerup! The key Powerup should spawn randomly just like the Ball Powerup and descend toward the bottom 
 of the screen just the same, where the Paddle has the chance to collide with it and pick it up. You’ll need to take 
